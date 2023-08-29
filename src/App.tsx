4dceee0d50273dd63cc25583f9c3ef9318e0ffad
NovaSagittarii <novasagittarii@gmail.com>
2023-08-28 19:51:20 -0700

    clear result after 5000ms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [payload, setPayload] = useState('');
   const tokens = payload.match(/\^(.*?) *\^[0-9]*?0+([0-9]+) 000 /) || [];
   const result = [...tokens].slice(1);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPayload('');
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [payload]);
 
   return (
     <>
