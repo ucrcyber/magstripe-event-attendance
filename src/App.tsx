95a44929f1a470f5344a6336b077885ac59977c6
NovaSagittarii <novasagittarii@gmail.com>
2023-09-10 11:46:44 -0700

    add api interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,66 @@
-import { useEffect, useState } from 'react'
+import { useRef } from 'react'
+import { SnackbarProvider, enqueueSnackbar } from 'notistack';
 import './App.css'
 
 function App() {
-  const [payload, setPayload] = useState('');
-  const tokens = payload.match(/\^(.*?) *\^[0-9]*?0+([0-9]+) 000 /) || [];
-  const result = [...tokens].slice(1);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setPayload('');
-    }, 5000);
-    return () => clearTimeout(timer);
-  }, [payload]);
-
+  const ref = useRef<HTMLInputElement>(null);
   return (
     <>
-      <input onKeyDown={(e) => {
-        if(e.code.includes('Enter')){
-          setPayload((e.target as HTMLInputElement).value);
-          (e.target as HTMLInputElement).value = '';
-        }
-      }}></input>
-      {payload && (
-        <>
-          <div>{payload}</div>
-          <div>{result.join(' ')}</div>
-        </>
-      )}
+      <SnackbarProvider maxSnack={10}/>
+      <div className='background' onClick={() => ref.current && ref.current.focus()}>
+        {'Click anywhere to start scanning'}
+        <input ref={ref} onKeyDown={(e) => {
+          if(e.code.includes('Enter')){
+            const payload:string = (e.target as HTMLInputElement).value;
+            (e.target as HTMLInputElement).value = '';
+
+            const tokens = payload.match(/\^(.*?) *\^[0-9]*?0+([0-9]+) 000 /) || [];
+            const result = [...tokens].slice(1);
+            const name: string = (result[0] || '/').split('/').reverse().join(' ')
+              .toLowerCase().split(' ').map(x => x.replace(/./, (a) => a.toUpperCase())).join(' ');
+            const id: string = result[1] || '';
+            // 'name': _name,
+            //     'id': _id,
+            //     'raw_card_data': _raw_card_data,
+            if(name && id){
+              // enqueueSnackbar(`${name} (${id})`, {
+              //   variant: 'info',
+              //   autoHideDuration: 5000,
+              // });
+              fetch('api/checkin', {
+                headers: {
+                  'Accept': 'application/json, text/plain',
+                  'Content-Type': 'application/json;charset=UTF-8'
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                  name, id,
+                  raw_card_data: payload,
+                }),
+              }).then(async response => {
+                const ack = await response.json();
+                console.log(ack);
+                if(ack && ack.message === "Success"){
+                  enqueueSnackbar(`${name} (${id}) saved`, {
+                    variant: 'success',
+                    autoHideDuration: 5000,
+                  });
+                }else{
+                  enqueueSnackbar(ack.error || `${name} (${id}) did not save`, {
+                    variant: 'error',
+                    autoHideDuration: 5000,
+                  });
+                }
+              })
+            }else{
+              enqueueSnackbar('Unable to scan', {
+                variant: 'error',
+                autoHideDuration: 5000,
+              });
+            }
+          }
+        }}></input>
+      </div>
     </>
   )
 }
